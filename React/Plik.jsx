19e37a3620c994507fabd98a57a73eac3fc4ebce
ZABA78 <127852755+ZABA78@users.noreply.github.com>
2025-01-08 08:33:42 +0100

    Add files via upload

---
+++ React/Plik.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const App = () => (
+    <div>
+    <div className="countdown">
+        <strong>Śniadanie</strong> - 7:00
+    </div>
+       <div className="countdown">
+       <strong>Obiad</strong> - 15:00
+   </div>
+   </div>
+);
+
+export default App;
